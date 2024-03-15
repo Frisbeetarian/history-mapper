d2360c22113e7d9ce7b49eac32fbfb56b0a5ea1b
Muhammad Sulayman Haydar <mohamad.sleimanhaidar@gmail.com>
2024-03-15 14:18:39 +0200

    use different tile

--- src/components/MapComponent.tsx
+++ src/components/MapComponent.tsx
@@ -17,8 +17,8 @@ export default function MapComponent() {
       style={{ height: '100vh', width: '100%' }}
     >
       <TileLayer
-        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        attribution='<a href="https://github.com/cyclosm/cyclosm-cartocss-style/releases" title="CyclOSM - Open Bicycle render">CyclOSM</a> | Map data: &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+        url="https://{s}.tile-cyclosm.openstreetmap.fr/cyclosm/{z}/{x}/{y}.png"
       />
       <GeoJSON data={turkeyCitiesBorders} onEachFeature={onEachFeature} />
     </MapContainer>
