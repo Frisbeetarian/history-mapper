846f751b833e9d638f0228612e6c82911f02ccc3
Muhammad Sulayman Haydar <mohamad.sleimanhaidar@gmail.com>
2024-03-15 10:33:45 +0200

    add initial overlay

--- src/components/MapComponent.tsx
+++ src/components/MapComponent.tsx
@@ -1,23 +1,26 @@
 import React from 'react'
-import { MapContainer, Marker, Popup, TileLayer } from 'react-leaflet'
+import { MapContainer, Marker, Popup, TileLayer, GeoJSON } from 'react-leaflet'
 import 'leaflet/dist/leaflet.css'
-const MapComponent = () => (
-  <MapContainer
-    center={[51.505, -0.09]}
-    zoom={8}
-    scrollWheelZoom={true}
-    style={{ height: '100vh', width: '100%' }}
-  >
-    <TileLayer
-      attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-      url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-    />
-    <Marker position={[151.505, -10.09]}>
-      <Popup>
-        A pretty CSS3 popup. <br /> Easily customizable.
-      </Popup>
-    </Marker>
-  </MapContainer>
-)
+import turkeyCitiesBorders from '../geojsons/turkey/tr-cities.json'
+export default function MapComponent() {
+  const onEachFeature = (feature, layer) => {
+    if (feature.properties && feature.properties.name) {
+      layer.bindPopup(feature.properties.name)
+    }
+  }
 
-export default MapComponent
+  return (
+    <MapContainer
+      center={[39, 35]}
+      zoom={6}
+      scrollWheelZoom={true}
+      style={{ height: '100vh', width: '100%' }}
+    >
+      <TileLayer
+        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+      />
+      <GeoJSON data={turkeyCitiesBorders} onEachFeature={onEachFeature} />
+    </MapContainer>
+  )
+}
